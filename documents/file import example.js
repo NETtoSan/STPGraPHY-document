27797fd867872413fba7dddd1b6a88acbf941c64
NETtoSan <62388769+NETtoSan@users.noreply.github.com>
2020-04-25 12:28:20 +0700

    Update file import example.js

--- documents/file import example.js
+++ documents/file import example.js
@@ -2,20 +2,20 @@ const Discord = require('discord.js')
 const bot = new Discord.CLient()
 const token = 'YOUR_BOT_TOKEN'
 
-##Your local system integration
+//Your local system integration
 const fs = require('fs')
 
 bot.login
 
-##This is how you set up file import codes##
+//This is how you set up file import codes
 
 bot.commands = new Discord.Collection //The keyword "commands" can be anything you like to name
 
-##Read files from the directory
+//Read files from the directory
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))      //The directory can be any directory you like And the code filter(file => ) you can filter whatever you like
 
-##For setting up commands##
+//For setting up commands
 
 for (const file of commandFiles){
 const command = require(`./commands/${file}`); // Can be any directory you want
