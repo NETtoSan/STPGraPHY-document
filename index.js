14a692f21be4c56c615288c108c3d92aff173912
NETtoSan <62388769+NETtoSan@users.noreply.github.com>
2020-05-11 06:18:21 +0700

    Update index.js
    New javascript file for Discord bot. Performance optimization and suitable for students and custom bot hosting

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //INITIZE BOT SEQUENCE AND LOGIN. GETTING BOT ONLINE
 const Discord = require('discord.js')
+const fs = require('fs')
 var bot = new Discord.client
 const PREFIX = '>!'
 const token = 'YOUR_TOKEN_HERE'
@@ -13,27 +14,44 @@ bot.on('ready',() =>{
     })
 
 //LET BOT READ YOUR MESSAGES
-bot.on('message', async message =>{
-    let args = message.content.subString(PREFIX.length).split(" ")
-    
+bot.on('message' , async message =>{
+    let args = message.content.substring(PREFIX.length).split(" ")
     switch(args[0]){
-    
-    case 'test':
-       message.reply('hello!)
-       break;
-    case 'hello':
-       message.reply('hello!')
-       break;
+        case 'system' :
+            if(args[1] == 'browse'){
+                const filter = m => m.author.id = message.author.id
+                message.channel.send("Please enter the query")
+                message.channel.awaitMessages(filter , {max : 1 , time : 100000}).then(collected =>{
+                    if(collected.first().content){
+                        message.channel.send(`Containing files :
+                        fs.readdirSync(collected.first().content)
+                        `)
+                        
+                    }
+                }).catch(err => {
+                    console.log(err)
+                })
+            }
+            else if(args[1] == 'get'){
+                const filter = m=> m.author.id = message.author.id
+                message.channel.send("Please enter the query")
+                message.channel.awaitMessages(filter , {max : 1 , time : 100000}).then(collected =>{
+                    if(collected.first().content){
+                        const attachment = new Discord.messageAttachment()
+                        .setFile(collected.first().content)
+                        const embed = new Discord.messageEmbed()
+                        .setTitle("Download")
+                        .setDescription(`${message.author.username} Has downloaded ${collected.first().content`)
+                        message.channel.send(embed , attachment)
+                    }
+                }).catch(err => {
+                    console.log(err)
+                })
+                
+            }
+            else{
+                //You can set your own custom statement here
+            }
+    break;
     }
-    //STPGRAPHY COMMANDS GOES HERE
-    if(message.content.startsWith(PREFIX+"stpgraphy"){
-    
-    //PUT YOUR CUSTOM COMMANDS HERE
-    if(args[1] == 'test'){
-       message.reply("You bot is working correctly. Delete this code")
-    }
-    else{
-       message.reply("Type >!stpgraphy test")
-    }
-    }
-}
+})
