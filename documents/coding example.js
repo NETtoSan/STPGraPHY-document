794691f40692b26e91e84fdc0bc8208070b3cf09
NETtoSan <62388769+NETtoSan@users.noreply.github.com>
2020-05-11 14:13:30 +0700

    Update coding example.js

--- documents/coding example.js
+++ documents/coding example.js
@@ -4,21 +4,26 @@ const token  = 'YOUR_BOT_TOKEN'
 bot.login(token)
 
 bot.on('ready',() =>{
+      //Add something that tells you that the bot has go online
+      
       console.log(`${bot.user.tag} has logged on !`)
 })
 
 bot.on('message', async message => {
        let args = message.content.substring(PREFIX.length).split(" ")
        switch(args[0]){
-       ##You can put your own response code here.##
+       //You can add a custom commands here as a case
        
        case 'test':
+                   //Add the response you wanted to add associated with a case
        message.reply('OK')
-       break;   ##Dont forget to put the break code. Without it. it'll continue executing the rest of the code!##
+       break;   //Always add a break. Otherwise it'll loop through every commands
 }
-##You can do another way without switching args
+      
+//Alternative way to have a response
 
 message.content.startsWith("test!"){
+      //Add the response you wanted to add associated with a case
         message.reply('OK');
         break;
 }
